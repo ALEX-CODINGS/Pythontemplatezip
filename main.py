856e624373b22b61774a6b88eaf1a7c5faf5c6e6
Alex <111903@st-maartenscollege.nl>
2026-06-10 08:25:36 +0000

    verandering

--- main.py
+++ main.py
@@ -19,11 +19,47 @@ import random
 gekozen_woord = random.choice(woord_lijst)
 #de computer geeft de gebruiker 5 levens.
 aantal_levens = 5
-print( "je hebt nog "+ str(aantal_levens)+ "levens over om het woord te raden")
-print("de woord lengte is" + (len(gekozen_woord))
+print( "je hebt nog "+ str(aantal_levens)+ " levens over om het woord te raden")
+print("de woord lengte is " + str(len(gekozen_woord)))
 #de computer vraagt de gebruiker om een letter te in te voeren.
-print = input("kies een letter:")
+letter = input("kies een letter:")
 
+#lijst met streepjes defineren
+woordjes = ["_"] * len(gekozen_woord)
+
+#zorgt ervoor dat de letter op de juiste plek komt
+for i in range(len(gekozen_woord)):
+    if gekozen_woord[i] == letter:
+        woordjes[i] = letter
+        print("je hebt een letter goed geraden")
+else:
+    aantal_levens -= 1
+    print("Fout geraden!")
+    print("Je hebt nog", aantal_levens, "levens over.")
+if "_" not in woordjes:
+        print("goed gedaan! Je hebt het woord geraden!:", gekozen_woord)
+else:
+        print("Game over!")
+        print("Het woord was:", gekozen_woord) 
+#printen van de juiste letter op de juiste plek.
+
+
+    
+   
+print(" ".join(woordjes))
+
+
+
+
+
+
+
+
+
+    
+
+   
+    
 
 
 
